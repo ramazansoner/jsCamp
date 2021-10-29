a6ff2c6fa920c27d172b432207099f788f858697
ramazansoner <79145958+ramazansoner@users.noreply.github.com>
2021-10-29 07:21:57 +0300

    Jscript 1. Ders

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba kodlama io")
+
+let dolarDun = 9.20
+let dolarBugun= 9.30
+{
+    let dolarDun = "Ankara"
+}
+
+
+console.log(dolarDun)
+
+let konutKredileri =["Konut Kredisi", "Emlak Kredisi","Kamu Konut Kredisi"]
+
+console.log(konutKredileri)
