aac46d01fc7d73e907063925970f9d99b15c1923
ramazansoner <79145958+ramazansoner@users.noreply.github.com>
2021-11-28 23:41:14 +0300

    function

--- src/function.js
+++ src/function.js
@@ -9,12 +9,42 @@ addToCart(10) // elma yukarıda tanımlıydı.O nedenle sadece sayıyı yazmak i
 addToCart("ayakkabı",25)
 
 //--------------------------------
+// first method using func.  
 function addCustomer(customer){
     console.log("Müşteri eklendi: " + customer)
 }
 addCustomer("Ramazan")
 
+//using second method in func
 let sayHello=()=>{ // değişkeni tanımlıyoruz diyoruz ki sen bir fonk.sun ve bu senin değerin
-    console.log("Say Hello")
+    console.log("Hello Word")
 }
 sayHello()
+
+//third method in func.
+let sayHello2 = function () {
+    console.log("Hello World 2")
+}
+sayHello2()
+
+
+function addToCart2(productName,quantity,unitPrice){
+console.log("sepette " + productName + " " + quantity +"adetin fiyatı "+unitPrice*quantity +" tutmaktadır.")
+}
+addToCart2("Karpuz",10,5)
+//Fonksiyonalara sürekli böyle parametre göndermek neyi nerede tutulduğunu bilmek açısından zor olabilir.
+//Bu nedenle objelerden yararlanıyoruz.
+//Yukarıdaki 3 özelliği ayrı ayrı tanımlatan bir obje yaratıyoruz.
+console.log("---------------------------------------------")
+console.log("---------------------------------------------")
+function addToCart3(product) { //Bana bir değer gönder ben ona Ürün diyecem.
+     console.log("Ürün: " + product.productName)//bana bir veri gelecek ve bunun bir productName i olacak.
+     console.log("Adet: " +product.quantity)
+     console.log("Fiyat: "+product.unitPrice)   
+    }
+let product1 = {
+productName:"Elma",
+unitPrice:10,
+quantity:5
+}
+addToCart3(product1)
